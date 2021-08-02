6630f2387accca99427816e5fe3e4b71d0d9eeae
volbil <volbilnexus@gmail.com>
2021-08-03 00:03:31 +0300

    Disabled staking by default

--- src/miner.h
+++ src/miner.h
@@ -30,7 +30,7 @@ namespace Consensus { struct Params; };
 
 static const bool DEFAULT_PRINTPRIORITY = false;
 
-static const bool DEFAULT_STAKE = true;
+static const bool DEFAULT_STAKE = false;
 
 static const bool DEFAULT_STAKE_CACHE = true;
 
